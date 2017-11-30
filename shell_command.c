18721a2592edf53c32b3683030859b19675c9980
hongwilliam <willhong20@gmail.com>
2017-11-30 15:38:14 -0500

    Testing input to shell needs to be fixed first, refer to comments

--- shell_command.c
+++ shell_command.c
@@ -7,7 +7,6 @@
 #include <time.h>
 #include <sys/wait.h>
 #include <dirent.h>
-#include <ctype.h>
 
 #include "shell_command.h"
 //reminder: there can only be one main function!
@@ -27,7 +26,7 @@ char ** parse_args(char * line){
   int i = 0;
   while(i < sizeof(arguments)){
     //hint 1: assume all items on command line are to be separated by single space
-    arguments[i] = strsep(&line," ");
+    arguments[i] = strsep(&line, " ");
     i++;
   }
   arguments[i] = NULL;
@@ -39,26 +38,27 @@ char ** parse_args(char * line){
 -hint 2: when parsing the line, count number of tokens (individual pieces)
 */
 
-//tokens = separate commands, ';' seperates each commands
-//default: return 1 if no ';' is found
-int count_num_tokens(char * line){
+//tokens = distinct commands that are seperated by a seperator
+//default: return 1 if the seperator is not found; this means single command
+int count_num_tokens(char * line, char seperator){
   int n = 1;
   char * temp = line;
-  while ( (temp = strchr(temp, ';')) ){
+  while ( (temp = strchr(temp, seperator)) ){
     temp++;
     n++; }
   return n;
 }
 
-//return array of tokens (commands) seperated by ';'
-char ** parse_line(char * line){
-    char ** answer;
+//return array of tokens (commands) that are seperated by the seperator
+char ** parse_line(char * line, char seperator){
+
+    int num_tokens = count_num_tokens(line, seperator);
+    char ** answer = (char **)calloc(num_tokens, sizeof(char *));
+    char the_seperator[2];
+    the_seperator[0] = seperator; //singular character
+    the_seperator[1] = 0; //terminating null
+
     int i = 0;
-    int num_tokens = 0;
-
-    num_tokens = count_num_tokens(line);
-    answer = (char **)calloc(num_tokens, sizeof(char *));
-
     //allocate memory
     while (i < num_tokens){
       answer[i] = (char *)malloc(512);
@@ -67,11 +67,11 @@ char ** parse_line(char * line){
     i = 0;
     //populate array
     while (i < num_tokens){
-      answer[i] = strsep(&line,";");
+      answer[i] = strsep(&line, the_seperator);
       i++; }
 
     //terminating NULL
-    answer[i] = 0;
+    answer[num_tokens] = 0;
     return answer;
 }
 
@@ -80,12 +80,12 @@ char ** parse_line(char * line){
 char * fix_format(char * line){
 
   //get rid of pesky spaces at front
-  while(isspace(*line)){
+  while(*line == ' '){
     line ++; }
 
   //get rid of pesky spaces at back
   char * back = line + strlen(line) - 1;
-  while (line < back && isspace(*back) ){
+  while (line < back && (*back == ' ')){
       back --; }
 
   //place terminating null
@@ -102,7 +102,7 @@ char * fix_format(char * line){
 
 //redirect file descriptor std to new file
 //create new file if nonexistent
-//return fie descriptor for redirected file
+//return file descriptor for redirected file
 int redirect(int std, int *original, char * file){
   //store original value of std
   *original = dup(std);
@@ -121,47 +121,119 @@ int redirect(int std, int *original, char * file){
 
 //redirect file descriptor std to original file
 //literally straight from class notes
-void redirect_back(int fd, int std, int original){
+int redirect_back(int std, int original, int fd){
   dup2(original, std);
-  close(fd); }
+  close(fd);
+  return 0; }
 
 //new and improved
+//to be impleneted later: handle exit and cd differently
+//each argument seperated by ' '
 void exec_command(char * line){
 
   //setup
   line = fix_format(line);
-  int tokens = count_num_tokens(line);
-  char ** command_arr = parse_line(line);
+  int tokens = count_num_tokens(line, ' ');
+  char ** command_arr = parse_line(line, ' ');
   int command;
   int status;
 
-
-  /**
-  //special case: if user entered cd
-  if ( strncmp(command_arr[0], "cd", sizeof(command_arr[0]) ) == 0){
-    chdir(command_arr[1]);
+  //fork and then exec the command
+  int f;
+  f = fork();
+  if (f == 0){
+    execvp(command_arr[0], command_arr);
     //deal with errors later
   }
-
   else{
-  */
-    //fork and then exec the command
-    int f;
-    f = fork();
-    if (f == 0){
-      execvp(command_arr[0], command_arr);
-      //deal with errors later
-    }
-    else{
-      f = wait(&status); }
+    f = wait(&status); }
+}
 
+//process shell input commands to be executed
+//seperate each individual command by ; and executes each
+//handle redirection and implement pipes if applicable
+void process_input(char * line){
+  //setup
+  int num_tokens = count_num_tokens(line, ';'); //this is number of commands
+  int in_fd = 0;
+  int out_fd = 0;
+  int in_copy = 0;
+  int out_copy = 0;
 
+  int i = 0;
+  char copy[512];
+  char pipe_file[512];
+  char *s;
+  char *temp;
+  while (i < num_tokens){
+      //seperate each individual command by ;
+      strncpy(copy, strsep(&line, ";"), sizeof(copy) );
+      s = copy;
+
+      //redirect stdin?
+      if (strchr(s, '<') != 0){ //if there exists < in the Input
+        strsep(&s, "<"); //seperate token <
+        in_fd = redirect(STDIN_FILENO, &in_copy, s);
+        //refer to commenets on redirect function
+      }
+
+      //redirect stdout?
+      if (strchr(s, '>') != 0){
+        strsep(&s, ">");
+        out_fd = redirect(STDOUT_FILENO, &out_copy, s); }
+
+      //implement pipe? (limit to single pipe)
+      if (strchr(s, '|') != 0){
+        //redirect first command to temp
+        temp = strsep(&s, "|");
+        strncpy(pipe_file, ".tempfile", sizeof(pipe_file) );
+        out_fd = redirect(STDOUT_FILENO, &out_copy, pipe_file);
+        exec_command(temp); //execute the command
+
+        //redirect stdin to temp
+        in_fd = redirect(STDIN_FILENO, &in_copy, pipe_file);
+        exec_command(s);
+
+        //revert stdout
+        out_fd = redirect_back(STDOUT_FILENO, out_copy, out_fd);
+        remove(".tempfile"); }
+
+      //no implementation of pipes desired
+      else{
+        exec_command(s);
+
+        //if in_fd initialized, then revert back from its redirection
+        if (in_fd){
+          in_fd = redirect_back(STDIN_FILENO, in_copy, in_fd); }
+
+        //if out_fd initialized, then revert back from its redirection
+        if (out_fd){
+          out_fd = redirect_back(STDOUT_FILENO, out_copy, out_fd); }
+
+      }
+  }
 }
 
 
-
-
 int main(){
-  char line2[] = "ls ; man chdir";
-  printf("\nnumber of tokens in 'ls ; man chdir': %d \n", count_num_tokens(line2));
+  //testing input
+  char *s;
+  printf("Input commands here: ");
+  s = fgets(s, sizeof(s),stdin);
+  printf("Inputted: %s", s); //immediate feedback
+
+  int tokens = count_num_tokens(s, ' ');
+  char ** args = parse_line(s, ' ');
+  printf("Number of tokens: %d \n", tokens);
+
+  int i = 0;
+  while(i < tokens){
+    printf("Command: %s \n", args[i]); //print out each command
+    execvp(args[0], args);//execute each command
+    i++;
+  }
+
+
+  //char line2[] = "ls ;man chdir";
+  //printf("\nnumber of tokens in 'ls ; man chdir': %d \n", count_num_tokens(line2, ' '));
 }
--- shell_command.h
+++ shell_command.h
@@ -1,7 +1,8 @@
 char ** parse_args(char * line);
 void exec_command(char * line);
-int count_num_tokens(char * line);
-char ** parse_line(char * line);
-char * fix_format(char * line)
+int count_num_tokens(char * line, char seperator);
+char ** parse_line(char * line, char seperator);
+char * fix_format(char * line);
 int redirect(int std, int *original, char * file);
-void redirect_back(int fd, int std, int original)
+int redirect_back(int fd, int std, int original);
+void process_input(char * line);
