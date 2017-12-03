7418f4b5e24de5be644c737b6b2ba2a7235f1781
Anthony Hom <anthonyhom2@hotmail.com>
2017-12-03 00:13:22 -0500

    Updated h file

--- shell_command.h
+++ shell_command.h
@@ -1,6 +1,6 @@
 void exec_command(char * line);
-int count_num_tokens(char * line, char seperator);
-char ** parse_line(char * line, char seperator);
+int count_num_tokens(char * line, char separator);
+char ** parse_line(char * line, char separator);
 char * fix_format(char * line);
 int redirect(int std, int *original, char * file);
 int redirect_back(int fd, int std, int original);
