0316f6141a574129013096f36ae467320c2bb57c
hongwilliam <willhong20@gmail.com>
2017-12-04 09:38:32 -0500

    Small formatting errors

--- shell_command.h
+++ shell_command.h
@@ -3,5 +3,4 @@ int count_num_tokens(char * line, char separator);
 char ** parse_line(char * line, char separator);
 char * fix_format(char * line);
 int redirect(int std, int *original, char * file);
-int redirect_back(int fd, int std, int original);
 void process_input(char * line);
